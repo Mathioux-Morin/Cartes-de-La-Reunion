dbba9d7a50bae69e09d28dcd2ef0ca2ffd67a97a
Supermorin <38919240+Mathioux-Morin@users.noreply.github.com>
2025-09-19 19:21:21 +0200

    Nouveaux marqueurs personnalisés

--- Site circuit randonnées/assets/js/script.js
+++ Site circuit randonnées/assets/js/script.js
@@ -6,16 +6,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Nouvelle icône personnalisée
+var pinIconRando = L.icon({
+    iconUrl: 'assets/images/icones/pinRandonnee.png',
+    iconSize: [128, 128], // Taille de l'icône
+    iconAnchor: [64, 90], // Point d'ancrage de l'icône
+    popupAnchor: [0, -48] // Position du popup par rapport à l'icône
+});
+
 datas.forEach(
-    (data) => {
-        if (data.location != null) {
-        //Ajout des marqueurs
-        L.marker([data.location.lat, data.location.lon]).addTo(map)
-        .bindPopup("<b> Nom : </b>" + data.nom + 
-            "</br><b>Durée : </b>" + data.duree_minutes_total + " min</br>" +
-            "<b>Zone : </b>" + data.zone_translations_item_nom +
-            "</br><b>Sentier ouvert :</b>" + data.is_ouvert)
-        .openPopup();
-    }
-    }
+    (data) => 
+        {
+        if (data.location != null)
+            {
+            //Ajout des marqueurs avec l'icône personnalisée
+            L.marker([data.location.lat, data.location.lon], { icon: pinIconRando }).addTo(map)
+            .bindPopup("<b> Nom : </b>" + data.nom + 
+                "</br><b>Durée : </b>" + data.duree_minutes_total + " min</br>" +
+                "<b>Zone : </b>" + data.zone_translations_item_nom +
+                "</br><b>Sentier ouvert :</b>" + data.is_ouvert)
+            .openPopup();
+            }
+        }
 );
--- Site formation parcours sup/assets/js/script.js
+++ Site formation parcours sup/assets/js/script.js
@@ -6,14 +6,23 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Nouvelle icône personnalisée
+var pinIconRando = L.icon({
+    iconUrl: 'assets/images/icones/pinFormation.png',
+    iconSize: [128, 128], // Taille de l'icône
+    iconAnchor: [64, 90], // Point d'ancrage de l'icône
+    popupAnchor: [0, -48] // Position du popup par rapport à l'icône
+});
+
 datas.forEach(
-    (data) => {
-        //Ajout des marqueurs
-        L.marker([data.etab_gps.lat, data.etab_gps.lon]).addTo(map)
+    (data) => 
+        {
+        //Ajout des marqueurs avec l'icône personnalisée
+        L.marker([data.etab_gps.lat, data.etab_gps.lon], { icon: pinIconRando }).addTo(map)
         .bindPopup("<b> Nom de formation: </b>" + data.fl[0] + 
             "</br><b>Nom de l'établissement : </b>" + data.etab_nom +
             "<b>Commune : </b>" + data.commune +
             "</br><b>Type d'établissement :</b>" + data.tc)
         .openPopup();
-    }
+        }
 );
